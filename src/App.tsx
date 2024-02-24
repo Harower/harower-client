adbe153e0f25133b3046ad30fc4a9235cbee9f7d
Onyewuchi Emeka <marrnuel123@gmail.com>
2024-02-24 14:56:02 +0100

    feat(tailwind): included tailwind as the styling technology

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import { useState } from 'react';
-import viteLogo from './assets/vite.svg';
-import reactLogo from './assets/react.svg';
-import './App.css';
-
-function App() {
-  const [count, setCount] = useState(0);
-  console.log('hello world');
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(prevCount => prevCount + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="text-center bg-black items-center justify-center h-screen flex">
+      <p className="text-gray-100 text-3xl">Hello</p>
+    </div>
   );
-}
+};
 
 export default App;

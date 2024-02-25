b03750bbd0d4584f36c2c5455a0529a78529f6fa
Onyewuchi Emeka <marrnuel123@gmail.com>
2024-02-25 17:06:37 +0100

    feat(button component): added a dynamic button component

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,11 +1,35 @@
 import AppLogo from '../components/ui/AppLogo.tsx';
+import UiButton from '../components/ui/UiButton.tsx';
 import Loader from './Loader.tsx';
 
 export default function AuthLayout() {
   return (
-    <div className="bg-gray20 h-screen">
+    <div className="bg-white h-screen">
       <AppLogo />
-      <Loader size="s" variant="primary" />
+      <div className="flex flex-col space-y-4 w-1/2 bg-gray50 px-10 py-10 items-center justify-center">
+        <UiButton variant="primary" size="s" loading={false} isFullWidth textCasing="uppercase">
+          hello
+        </UiButton>
+        <UiButton variant="secondary" size="s" loading={true} isFullWidth textCasing="lowercase">
+          hello
+        </UiButton>
+        <UiButton variant="tertiary" size="md" loading={true} textCasing="capitalize">
+          hello
+        </UiButton>
+        <UiButton variant="danger" size="md" loading={false} textCasing="uppercase">
+          hello
+        </UiButton>
+        <UiButton variant="neutral" size="large" loading={true} textCasing="uppercase">
+          hello
+        </UiButton>
+        <UiButton variant="iconNeutral" size="large" loading={false} textCasing="uppercase">
+          hello
+        </UiButton>
+        <UiButton variant="primaryText" size="md" loading={false} textCasing="lowercase">
+          hello
+        </UiButton>
+        <Loader size="large" variant="primary" />
+      </div>
     </div>
   );
 }

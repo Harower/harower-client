4139491cdce5bbb2889dbb892666a2cd6476726f
Onyewuchi Emeka <marrnuel123@gmail.com>
2024-02-25 13:32:27 +0100

    feat(app logo): added app logo

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,8 +1,10 @@
+import AppLogo from '../components/ui/AppLogo.tsx';
 import Loader from './Loader.tsx';
 
 export default function AuthLayout() {
   return (
     <div className="bg-gray20 h-screen">
+      <AppLogo />
       <Loader size="s" variant="primary" />
     </div>
   );
